ff1f8cf3a404ee92284e359148461d80a879ee9c
Guy Bertental <guybartal@gmail.com>
2019-10-23 13:58:11 +0300

    style linting

--- upload.py
+++ upload.py
@@ -6,18 +6,20 @@ import yaml
 import math
 import datetime
 
+
 def Usage():
-    print (f'{sys.argv[0]} source-path')
+    print(f'{sys.argv[0]} source-path')
     sys.exit(1)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     if len(sys.argv) != 2:
         Usage()
 
     src_path = sys.argv[1]
 
-    if not os.path.exists(src_path): 
-        print (f'path not exists: {src_path}')
+    if not os.path.exists(src_path):
+        print(f'path not exists: {src_path}')
         sys.exit(1)
 
     with open("settings.yml", 'r') as ymlfile:
@@ -27,24 +29,23 @@ if __name__=='__main__':
     print(f'connecting to custom vision project')
     trainer = CustomVisionTrainingClient(settings['training_key'], endpoint=settings['endpoint'])
 
-    project = trainer.get_project(settings['project_id']) # project id
+    project = trainer.get_project(settings['project_id'])
     print(f'connected to project: {project.name}')
 
-    print (f'listing images in path: {src_path}')
-    
+    print(f'listing images in path: {src_path}')
+
     files = os.listdir(src_path)
-    
+
     batch_size = int(settings['batch_size'])
     batches = math.ceil(len(files) / batch_size)
-    print (f'found {len(files)} files to upload in {batches} batches, batch size set to {batch_size}')
+    print(f'found {len(files)} files to upload in {batches} batches, batch size set to {batch_size}')
 
     for batch in range(0, batches):
         timestamp = datetime.datetime.now()
-        print (f'loading batch #{batch}')
+        print(f'loading batch #{batch}')
         imageEntries = []
         for index in range(batch * batch_size, min((batch + 1) * batch_size, len(files))):
             file_name = files[index]
-            #print (f'loading file: {file_name}, index {index}')
             with open(os.path.join(src_path, file_name), mode="rb") as image_contents:
                 imageEntries.append(ImageFileCreateEntry(name=file_name, contents=image_contents.read(), regions=[]))
     
@@ -56,5 +57,5 @@ if __name__=='__main__':
                 print(f"image {image.source_url} failed with status: {image.status}")
             #exit(-1) # todo: handle retry
         batch_time = (datetime.datetime.now() - timestamp).seconds
-        print (f'finished batch #{batch}, took {batch_time} seconds. estimated time left: {(batches - batch - 1) * batch_time} seconds')
+        print(f'finished batch #{batch}, took {batch_time} seconds. estimated time left: {(batches - batch - 1) * batch_time} seconds')
     print(f'done, took {(datetime.datetime.now() - start).seconds} seconds.')
